89c79f97437f28197dd4d3b13df334047ed810a2
Mike Irvine <michael.irvine.mai@googlemail.com>
2015-07-05 16:19:42 +0100

    Basic interface with some admin control.

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,48 @@
+class WordsController < ApplicationController
+	before_action :authenticate_admin!
+	skip_before_action :authenticate_admin!, only: [:index, :show]
+	def index
+		@words = Word.all
+	end
+	def new
+		@word = Word.new
+
+	end
+	def create
+  		@word = Word.new(word_params)
+
+  		if @word.save
+  			redirect_to word_path(@word)
+  		else
+  			render 'new'
+  		end
+	end
+	def show
+		@word = Word.find(params[:id])
+	end
+
+	def edit
+		@word = Word.find(params[:id])
+	end
+
+	def update
+		@word = Word.find(params[:id])
+		if @word.update(word_params)
+			redirect_to word_path(@word)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@word = Word.find(params[:id])
+		@word.destroy
+
+		redirect_to words_path
+	end
+
+	private
+		def word_params
+			params.require(:word).permit(:name, :description)
+		end
+end
---
+++ app/models/word.rb
@@ -0,0 +1,4 @@
+class Word < ActiveRecord::Base
+	validates :name, presence: true, uniqueness: true
+	validates :description, presence: true
+end
